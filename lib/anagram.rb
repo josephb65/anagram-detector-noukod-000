50bf45d7993868abcf1d2f6274a5fe7080ea5e1e
Billy Joseph <ingbillyjoseph@gmail.com>
2019-04-24 16:18:12 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,4 +5,13 @@ class Anagram
  def initialize(wrd)
     @wrd = wrd
   end
+   def initialize(wrd)
+    @wrd = wrd
+  end
+
+  def match(word_array)
+    word_array.select do |word|
+      word.split("").sort == @wrd.split("").sort
+    end
+  end
 end
